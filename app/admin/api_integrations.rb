c141baf937aef619450feee4b89691e9c8b62848
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-13 22:32:15 +0000

    Whitelist columns in ApiIntegration for ActiveAdmin

--- app/admin/api_integrations.rb
+++ app/admin/api_integrations.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ApiIntegration do
 
+ permit_params :documentation, :purpose, :project_id, :example_endopoint
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
