07872aaea99bcdf8337849b3aef4dfd1f7d9d540
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-13 22:31:38 +0000

    Direct association between projects and api_integrations

--- app/models/api_integration.rb
+++ app/models/api_integration.rb
@@ -1,6 +1,8 @@
 class ApiIntegration < ApplicationRecord
   # Direct associations
 
+  belongs_to :project
+
   # Indirect associations
 
   # Validations
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,9 @@
 class Project < ApplicationRecord
   # Direct associations
 
+  has_many   :api_integrations,
+             :dependent => :destroy
+
   has_many   :user_stories,
              :dependent => :destroy
 
