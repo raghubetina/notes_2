32cdd11d668d5a07403881312ad6db1daf59b6fa
Raghu Betina <raghu.betina@chicagobooth.edu>
2021-08-13 22:32:00 +0000

    Whitelist columns in Project for ActiveAdmin

--- app/admin/projects.rb
+++ app/admin/projects.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Project do
 
+ permit_params :name, :existing_process, :job_to_be_done, :struggling_moment, :other, :existing_database, :user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
